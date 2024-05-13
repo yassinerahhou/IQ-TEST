9295b0b3eff71041d57c967c3a802860c899f5ce
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-05-13 14:49:44 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer/footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/footer.tsx
@@ -0,0 +1,56 @@
+import "./footer-style.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="parrent-div">
+          <div className="logo">
+            <h1>IQ TEST</h1>
+            <span id="social-media">
+              <i className="fab fa-facebook-f"></i>
+              <i className="fab fa-twitter"></i>
+              <i className="fab fa-instagram"></i>
+              <i className="fab fa-linkedin-in "></i>
+            </span>
+          </div>
+          <div className="quick_links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">NEWS</a>
+              </li>
+              <li>
+                <a href="#">BLOG</a>
+              </li>
+              <li>
+                <a href="#">IQ</a>
+              </li>
+            </ul>
+          </div>
+          <div className="LEGAL-PAGES">
+            <h3>Legal Pages</h3>
+            <ul>
+              <li>
+                <a href="#">Terms & Conditions</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <center>
+          {" "}
+          <p id="copyright">
+            &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+          </p>
+        </center>
+      </footer>
+    </>
+  );
+}
