327d9878ea10baf296398203ff777870a83a29c0
DepressedPenguin <125665615+DepressedPenguin@users.noreply.github.com>
2024-05-18 16:11:25 +0200

    some modfication

--- src/Components/Slider/index.tsx
+++ src/Components/Slider/index.tsx
@@ -7,28 +7,30 @@ import img_brain from "../../../src/assets/slider_imgs/brx.png";
 export default function Slider() {
   return (
     <>
-      <div className={styles.main_slider}>
-        <div className={styles.left_slider}>
-          <div className={styles.box_intro_headline}>
-            <h1>Unveil your genuine IQ score within a mere 2 minutes!</h1>
+      <div className={styles.main_parent}>
+        <div className={styles.main_slider}>
+          <div className={styles.left_slider}>
+            <div className={styles.box_intro_headline}>
+              <h1>Unveil your genuine IQ score within a mere 2 minutes!</h1>
+            </div>
+            <div className={styles.box_intro_takingtest}>
+              <p className={styles.p_random_test}>
+                600 Ppeople are taking the test right now.
+              </p>
+            </div>
+            <div className={styles.box_intro_button}>
+              <button className={`${styles.btn_start} btn btn-light`}>
+                Start certiificate test{" "}
+              </button>
+            </div>
           </div>
-          <div className={styles.box_intro_takingtest}>
-            <p className={styles.p_random_test}>
-              600 Ppeople are taking the test right now.
-            </p>
+          <div className={styles.right_slider_img}>
+            <img
+              className={styles.img_slider_class}
+              src={img_brain}
+              alt="img brain"
+            />
           </div>
-          <div className={styles.box_intro_button}>
-            <button className={`${styles.btn_start} btn btn-light`}>
-              Start certiificate test{" "}
-            </button>
-          </div>
-        </div>
-        <div className={styles.right_slider_img}>
-          <img
-            className={styles.img_slider_class}
-            src={img_brain}
-            alt="img brain"
-          />
         </div>
       </div>
     </>
