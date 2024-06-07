da631645f3f2b474cc000b2412e39769d105506e
DepressedPenguin <125665615+DepressedPenguin@users.noreply.github.com>
2024-06-07 23:32:28 +0200

    articles section added

--- src/Components/Pages/BLOG/index.tsx
+++ src/Components/Pages/BLOG/index.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import styles from "./blog.module.scss";
+import articles_data from "../../articles.json";
+
+// IMPORT IMG ARTICLE TEST
+import img_article_1 from "../../../assets/Articles_images/post_1.png";
 
 export default function Blog() {
   return (
-    <>
-      <h1 style={{ color: "#191919" }}>BLOG</h1>
-    </>
+    <div className={styles.articles}>
+      {articles_data.map((article, index) => {
+        return (
+          <div key={index} className={styles.card_article}>
+            <div className={styles.article_image}>
+              <img
+                className={styles.img_post_class}
+                src={article.image || img_article_1}
+                alt={`img post ${index + 1}`}
+              />
+            </div>
+            <div className={styles.less_info_article}>
+              <h1 className={styles.title_article}>{article.title}</h1>
+              <p>{article.para_article}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
