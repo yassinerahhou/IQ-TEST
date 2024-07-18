047432789d17337f52d43a74221f4425625db68f
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-07-18 14:59:28 +0100

    fix footer

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -1,75 +1,88 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import "./footer-style.css";
-import "@fortawesome/fontawesome-free/css/all.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
-    <>
-      <footer>
-        <div className="parrent-div">
-          <div className="logo">
-            <h1>IQ TEST</h1>
-            <span id="social-media">
-              <i className="iconSocial fab fa-facebook-f"></i>
-              <i className="iconSocial fab fa-twitter"></i>
-              <i className="iconSocial fab fa-instagram"></i>
-              <i className="iconSocial fab fa-linkedin-in "></i>
-            </span>
-          </div>
-          <div className="quick_links">
-            <h3>Quick Links</h3>
-            <ul>
-              <li>
-                <a className="a_remove_style" href="#">
-                  BLOGS
-                </a>
-              </li>
-              <li>
-                <a className="a_remove_style" href="#">
-                  KIDS
-                </a>
-              </li>
-              <li>
-                <Link to="/Start">
-                  <a className="a_remove_style" href="#">
-                    TEST
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="LEGAL-PAGES">
-            <h3>Legal Pages</h3>
-            <ul>
-              <li>
-                <a className="a_remove_style" href="#">
-                  Terms & Conditions
-                </a>
-              </li>
-              <li>
-                <a className="a_remove_style" href="#">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a className="a_remove_style" href="#">
-                  Contact Us
-                </a>
-              </li>
-              <li>
-                <Link to="/ABOUT">
-                  <span className="a_remove_style">ABOUT US</span>
-                </Link>
-              </li>
-            </ul>
+    <footer>
+      <div className="parrent-div">
+        <div className="logo">
+          <h1>IQ TEST</h1>
+          <div id="social-media">
+            <a href="#" className="iconSocial">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+            <a href="#" className="iconSocial">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+            <a href="#" className="iconSocial">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a href="#" className="iconSocial">
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
           </div>
         </div>
-
-        <div className="copyright_section">
-          <p id="copyright">
-            &copy; {new Date().getFullYear()} Your Company. All rights reserved.
-          </p>
+        <div className="quick_links">
+          <h3>Quick Links</h3>
+          <br />
+          <ul>
+            <li>
+              <a className="a_remove_style" href="#">
+                BLOGS
+              </a>
+            </li>
+            <li>
+              <a className="a_remove_style" href="#">
+                KIDS
+              </a>
+            </li>
+            <li>
+              <Link to="/Start" className="a_remove_style">
+                TEST
+              </Link>
+            </li>
+          </ul>
         </div>
-      </footer>
-    </>
+        <div className="LEGAL-PAGES">
+          <h3>Legal Pages</h3>
+          <br />
+          <ul>
+            <li>
+              <a className="a_remove_style" href="#">
+                Terms & Conditions
+              </a>
+            </li>
+            <li>
+              <a className="a_remove_style" href="#">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a className="a_remove_style" href="#">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <Link to="/ABOUT" className="a_remove_style">
+                ABOUT US
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="copyright_section">
+        <p id="copyright">
+          &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+        </p>
+      </div>
+    </footer>
   );
 }
