db35295545d8fe3e8e2002348cd545738eb0d7e8
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-07-24 01:58:20 +0100

    fix faq & blog page

--- src/Components/Pages/BLOG/index.tsx
+++ src/Components/Pages/BLOG/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import styles from "./blog.module.scss";
 import articles_data from "../../articles.json";
-import img_article_1 from "../../../assets/Articles_images/Post_1.png";
+import img_article_1 from "../../../assets/Articles_images/ost_1.png";
 
 interface Article {
   title: string;
