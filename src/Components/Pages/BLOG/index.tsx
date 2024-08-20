bd2ad5bcbd65072734b5dcee9ed27eeabab865bf
yassinerahhou <yassine.rahhou.1@gmail.com>
2024-08-20 16:07:09 +0100

    complete 25 articles

--- src/Components/Pages/BLOG/index.tsx
+++ src/Components/Pages/BLOG/index.tsx
@@ -23,38 +23,45 @@ const Blog: React.FC = () => {
   };
 
   return (
-    <div className={styles.blogContainer}>
-      <h1 className={styles.blogTitle}>Latest Articles</h1>
-      <div className={styles.articles}>
-        {articles.map((article: Article, index: number) => (
-          <Link
-            to={`/BlogArticle/${index}/${encodeURIComponent(article.title)}`}
-            key={index}
-            className={styles.articleLink}
-          >
-            <article className={styles.cardArticle}>
-              <div className={styles.articleImage}>
-                <img
-                  className={styles.imgPostClass}
-                  src={article.image_url}
-                  alt={`Article ${index + 1}`}
-                  loading="lazy"
-                />
-              </div>
-              <div className={styles.articleContent}>
-                <h2 className={styles.titleArticle}>{article.title}</h2>
-                <p
-                  className={styles.articleExcerpt}
-                  dangerouslySetInnerHTML={{
-                    __html: truncateText(article.para_article, 200),
-                  }}
-                />
-                <button className={styles.readMoreBtn}>Read More</button>
-              </div>
-            </article>
-          </Link>
-        ))}
-      </div>
+    <div className="page-container">
+      <main className="main-content">
+        <div className={styles.blogContainer}>
+          <h1 className={styles.blogTitle}>Latest Articles</h1>
+          <div className={styles.articles}>
+            {articles.map((article: Article, index: number) => (
+              <Link
+                to={`/BlogArticle/${index}/${encodeURIComponent(
+                  article.title
+                )}`}
+                key={index}
+                className={styles.articleLink}
+              >
+                <article className={styles.cardArticle}>
+                  <div className={styles.articleImage}>
+                    <img
+                      className={styles.imgPostClass}
+                      src={article.image_url}
+                      alt={`Article ${index + 1}`}
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className={styles.articleContent}>
+                    <h2 className={styles.titleArticle}>{article.title}</h2>
+                    <p
+                      className={styles.articleExcerpt}
+                      dangerouslySetInnerHTML={{
+                        __html: truncateText(article.para_article, 200),
+                      }}
+                    />
+                    <button className={styles.readMoreBtn}>Read More</button>
+                  </div>
+                </article>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="footer">{/* Footer content */}</footer>
     </div>
   );
 };
